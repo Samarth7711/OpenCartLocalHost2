08a0de313111fe938515d3c57362178b11817acd
Samarth Patil <samarth.patil@example.com>
2025-04-23 11:05:27 +0530

    second Commit

--- src/test/java/PageObjects/HomePage.java
+++ src/test/java/PageObjects/HomePage.java
@@ -1,5 +1,28 @@
 package PageObjects;
 
-public class HomePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage{
+	
+	public HomePage(WebDriver driver ) {
+		super(driver);	
+	}
+	
+	
+	@FindBy(xpath="//*[@id=\"top-links\"]/ul/li[2]/a")
+	WebElement lnkmyaccount;
+	
+	@FindBy(xpath="//*[@id=\"top-links\"]/ul/li[2]/ul/li[1]/a")
+	WebElement lnkregister;
+	
+	public void clickMyaccount() {
+		lnkmyaccount.click();
+}
+	public void clickregister() {
+		lnkregister.click();
+	}
+
 
 }
